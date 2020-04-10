15b8e9857d3fe1d74381a6267def84dd862aa922
Shema Bryan <shema.bryan@jjay.cuny.edu>
2020-04-10 02:23:40 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,8 +1,5 @@
 def valid_move?(board, index)
- if position_taken?( (board[index] == " ") || (board[index] == "") || (board[index] == nil)
-      return false
-   else
-     if index.between?(0,8) && !position_taken?(board, index)
-        return true
+   if index.between?(0,8) && !position_taken?(board, index)
+      return true
    end
 end
